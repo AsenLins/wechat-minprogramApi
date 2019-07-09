875740e9cce24a7995bc6b1641022dc31df65efd
asen <736239001@qq.com>
2019-07-10 01:01:58 +0800

    update

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ import login from './interface/login';
 import accessToken from './interface/accessToken';
 import pay from './interface/pay';
 import qr from './interface/qr';
+import units from './common/unit';
 
 export {
     login,
     accessToken,
     pay,
-    qr
+    qr,
+    units
 };
 
 export default {
     login,
     accessToken,
     pay,
-    qr
+    qr,
+    units
 };
